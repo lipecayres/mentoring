cad40a2206cf2ccb16b9494933d69dd8d862f828
Felipe Cayres <felipescayres@gmail.com>
2024-12-02 11:30:55 -0500

    activities update on sec 3

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/activities.js
@@ -0,0 +1,24 @@
+/* variables */
+
+const country = 'Canada';
+const continent = 'America';
+let population = 40_100_000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+// data types
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+// let, const and var
+
+language = 'English';
+
