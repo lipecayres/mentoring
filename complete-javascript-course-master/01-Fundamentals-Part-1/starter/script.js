daf48832c7aecaa46b60769fceec2c201ad00eec
Felipe Cayres <felipescayres@gmail.com>
2024-12-02 11:57:33 -0500

    lesson 18 + challenge 2

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,159 @@
+/*
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas')
+console.log(23)
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer'
+let job2 = 'teacher'
+
+console.log(myFirstJob)
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year)
+console.log(typeof year)
+
+year = 1991;
+console.log(typeof year)
+
+console.log(typeof null)
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+var job = 'programmer'
+job = `teacher`;
+
+lastName = 'Silva'
+console.log(lastName);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+
+console.log(ageJonas, ageSarah)
+
+console.log(ageJonas + 2, ageJonas/10, 2**3)
+
+const firstName = 'Jonas';
+const lastName = 'Silva';
+console.log(firstName + ' ' + lastName);
+
+// assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x*= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x)
+
+// comparison operators
+
+console.log(ageJonas>ageSarah); // <, > , <=, >=
+console.log(ageSarah>=18)
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018)
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah);
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year-birthYear) + ' years old ' + job + '!'
+console.log(jonas)
+
+const newJonas = `I'm ${firstName}, a ${year-birthYear} year old ${job}!`;
+console.log(newJonas)
+
+console.log(`Just a regular script...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+with multiple
+lines`)
+
+const age = 15;
+
+if(age >=18) {
+    console.log(`Sarah can start driving lessons`)
+} else {
+    const yearsLeft = 18 - age;
+console.log(`Sarah is too young. Wait another ${yearsLeft} years.`)
+}
+
+const birthYear = 1991;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century)
+
+let markHeight, markWeight, johnHeight, johnWeight;
+let markHigherBMI;
+
+markHeight = 1.69;
+markWeight = 78;
+johnHeight = 1.95;
+johnWeight = 92;
+
+let markBMI = markWeight / markHeight ** 2;
+let johnBMI = johnWeight / johnHeight ** 2;
+
+markBMI > johnBMI ? console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}!`) : console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
+
+*/
