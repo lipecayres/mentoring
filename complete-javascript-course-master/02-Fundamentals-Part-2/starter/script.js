39b3ade44148dc45686c509f09df2ff297cd06cc
Felipe Cayres <felipescayres@gmail.com>
2024-12-03 16:51:52 -0500

    challenge 2

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -84,10 +84,9 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(2,3))
-*/
 
 const calcAverage = (a, b, c) => {
-  return (a + b + c) / 3;
+    return (a + b + c) / 3;
 };
 
 let scoreDolphins = calcAverage(30, 30, 30);
@@ -98,9 +97,109 @@ function checkWinner(avgDolphins, avgKoalas) {
     console.log(`Dolphins wins (${avgDolphins} vs. ${avgKoalas})`);
   } else if (avgKoalas > 2 * avgDolphins)
     console.log(`Koalas wins (${avgDolphins} vs. ${avgKoalas})`);
-  else {
-    console.log(`No team wins`);
+    else {
+        console.log(`No team wins`);
   }
 }
 
 checkWinner(scoreDolphins, scoreKoalas);
+
+checkWinner(600, 214);
+
+const myFunc = function (a, b) {
+    return a + b;
+};
+
+
+// Arrays part 1
+
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020, 2018);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+// friends = ['Bob', 'Alice'] - ilegal redeclaration
+
+const firstName = "Jonas";
+const jonas = [firstName, "Silva", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+console.log(jonas.length);
+
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge2(years[0])
+const age2 = calcAge2(years[1])
+const age3 = calcAge2(years[years.length-1])
+console.log(age1, age2, age3)
+
+const age = [calcAge2(years[0]),calcAge2(years[1]), calcAge2(years[years.length-1])]
+
+
+//Arrays part 2
+
+const friends = ["Michael", "Steven", "Peter"];
+const newLenght = friends.push('Jay');
+console.log(friends)
+console.log(newLenght)
+
+friends.unshift('John');
+console.log(friends)
+
+// remove elements
+
+friends.pop() //last
+const popped = friends.pop()
+console.log(popped)
+console.log(friends)
+
+friends.shift(); // remove first
+console.log(friends)
+
+console.log(friends.indexOf('Steven'))
+console.log(friends.indexOf('Bob'))
+
+friends.push(23)
+console.log(friends.includes('Steven'))
+console.log(friends.includes('Bob'))
+console.log(friends.includes(23))
+
+if(friends.includes('Steven')) {
+  console.log('You have a friend called Steven')
+}
+
+// challenge 2
+
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 155, 44];
+const tip = [];
+const totals = [];
+
+bills.forEach((bill) => {
+  let eachTip = calcTip(bill) 
+  tip.push(eachTip);
+  totals.push(bill+eachTip)
+});
+
+console.log(tip)
+console.log(totals)
+
+*/
+
