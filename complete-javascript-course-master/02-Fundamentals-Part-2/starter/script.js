40586f810a78e17ed5ba5dab24826df124164ef3
Felipe Cayres <felipescayres@gmail.com>
2024-12-05 01:19:28 -0500

    lesson 47 done

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -342,5 +342,39 @@ for (let i = 0; i < 10; i++) {
 
 */
 
-
 // looping arrays
+
+const jonasArray = [
+  "jonas",
+  "silva",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+];
+const types = [];
+for (let i = 0, length = jonasArray.length; i < length; i++) {
+  types.push(typeof jonasArray[i]);
+  console.log(jonasArray[i], typeof jonasArray[i]);
+}
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+
+years.forEach((year) => {
+  console.log(2037 - year);
+});
+
+console.log('-----------------')
+for (let i = 0, length = jonasArray.length; i < length; i++) {
+  if (typeof jonasArray[i] !== "string") continue;
+
+  console.log(jonasArray[i], typeof jonasArray[i]);
+}
+
+console.log('------BREAK------')
+for (let i = 0, length = jonasArray.length; i < length; i++) {
+  if (typeof jonasArray[i] === "number") break;
+
+  console.log(jonasArray[i], typeof jonasArray[i]);
+}
+
