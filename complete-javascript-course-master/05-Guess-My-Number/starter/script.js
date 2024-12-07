ec389396ff17dd6998010794efd2e58393430438
Felipe Cayres <felipescayres@gmail.com>
2024-12-06 23:39:11 -0500

    game structure done

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,69 @@
 'use strict';
+
+const scoreMessage = document.querySelector('.score');
+const highscoreMessage = document.querySelector('.highscore');
+const message = document.querySelector('.message');
+const input = document.querySelector('.guess');
+const againButton = document.querySelector('.again');
+const checkButton = document.querySelector('.check');
+
+// display scores on screen
+let attempts = 20;
+let highscore = 0;
+scoreMessage.innerHTML = attempts;
+highscoreMessage.innerHTML = highscore;
+
+// Secret number to be guessed - 1 to 20
+let numberToBeGuessed = Math.trunc(Math.random() * 20 + 1);
+console.log(numberToBeGuessed)
+
+checkButton.addEventListener('click', checkNumber);
+againButton.addEventListener('click', playAgain)
+/*
+    Functions 
+ */
+
+// check if number is right, greater or smaller.
+function checkNumber() {
+    let x = Number(input.value);
+
+  // check out of range
+  if (x < 1 || x > 20) {
+      message.innerHTML = `Out of range, try again.`;
+      return;
+    }
+
+  // check number and range
+  if (x > numberToBeGuessed) {
+    message.innerHTML = `Too high!!`;
+    updateScore();
+  } else if (x < numberToBeGuessed) {
+      message.innerHTML = `Too low!!`;
+    updateScore();
+} else {
+    message.innerHTML = `You are correct!!`;
+    updateHighscore()
+}
+}
+
+function playAgain() {
+    input.value = '';
+    attempts = 20;
+    scoreMessage.innerHTML = attempts;
+    message.innerHTML = `Start guessing...`;
+    numberToBeGuessed = Math.trunc(Math.random() * 20 + 1);
+    console.log(numberToBeGuessed)
+}
+
+//update score on the screen
+function updateScore() {
+    attempts--;
+    scoreMessage.innerHTML = attempts;
+}
+
+function updateHighscore() {
+  if (attempts > highscore) {
+    highscore = attempts;
+    highscoreMessage.innerHTML = highscore;
+  }
+}
