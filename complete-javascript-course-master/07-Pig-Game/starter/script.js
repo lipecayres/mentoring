0073dc90a9bf331b321769e5148413d42c0804bb
Felipe Cayres <felipescayres@gmail.com>
2024-12-10 15:19:02 -0500

    defined init function

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -14,27 +14,43 @@ const btnNewGame = document.querySelector('.btn--new');
 const btnRollDice = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-/* Variables */
-let playing = true;
-let currentScore = 0;
-let activePlayer = 0;
-const scores = [0, 0];
+let scores, currentScore, playing, activePlayer;
+
+const init = function () {
+  /* Variables */
+  playing = true;
+  currentScore = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+
+  diceImageEl.classList.add('hidden');
+
+  scores.forEach(e => {
+    scores[e] = 0;
+  });
+
+  totalScorePlayer1El.textContent = scores[0];
+  totalScorePlayer2El.textContent = scores[1];
+  currentScorePlayer1El.textContent = 0;
+  currentScorePlayer2El.textContent = 0;
+
+  player1El.classList.remove('player--winner');
+  player2El.classList.remove('player--winner');
+  player1El.classList.add('player--active');
+  player2El.classList.remove('player--active');
+};
+init();
 
 /* Action to buttons */
-btnNewGame.addEventListener('click', newGame);
+btnNewGame.addEventListener('click', init);
 btnRollDice.addEventListener('click', rollDice);
 btnHold.addEventListener('click', hold);
-
-let diceNumber = 0;
-diceImageEl.classList.add('hidden');
-totalScorePlayer1El.textContent = 0;
-totalScorePlayer2El.textContent = 0;
 
 // Roll dice
 function rollDice() {
   if (!playing) return;
 
-  diceNumber = Math.trunc(Math.random() * 6) + 1;
+  let diceNumber = Math.trunc(Math.random() * 6) + 1;
 
   diceImageEl.src = `dice-${diceNumber}.png`;
   diceImageEl.classList.remove('hidden');
@@ -80,26 +96,6 @@ function hold() {
   } else {
     switchPlayer();
   }
-}
-
-function newGame() {
-  scores.forEach(e => {
-    scores[e] = 0;
-  });
-
-  playing = true;
-  totalScorePlayer1El.textContent = scores[0];
-  totalScorePlayer2El.textContent = scores[1];
-  currentScorePlayer1El.textContent = 0;
-  currentScorePlayer2El.textContent = 0;
-
-  diceImageEl.classList.add('hidden');
-  console.log(player1El);
-
-  player1El.classList.remove('player--winner');
-  player2El.classList.remove('player--winner');
-
-  player1El.classList.add('player--active');
 }
 
 function switchPlayer() {
