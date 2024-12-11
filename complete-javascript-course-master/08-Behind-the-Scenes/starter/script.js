258bc7de06198dc1aaec0f7abc7df443bd096f72
Felipe Cayres <felipescayres@gmail.com>
2024-12-11 12:12:30 -0500

    shallow and deep copy

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+    family: ['Alice', 'Bob']
+}
+/* 
+// Shalow copy
+const jessicaCopy = {...jessica}
+jessicaCopy.lastName = 'Davis'
+
+console.log(jessica, jessicaCopy)
+console.log('---------------------------')
+
+jessicaCopy.family.push('Mary')
+jessicaCopy.family.push('John')
+console.log(jessica, jessicaCopy)
+
+*/
+// Deep clone/copy
+
+const jessicaClone = structuredClone(jessica)
+
+jessicaClone.lastName = 'Davis'
+
+console.log(jessica, jessicaClone)
+console.log('---------------------------')
+
+jessicaClone.family.push('Mary')
+jessicaClone.family.push('John')
+console.log(jessica, jessicaClone)
