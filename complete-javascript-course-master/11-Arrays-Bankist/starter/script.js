2015b6e3a86152da8edcccf6a8bc528f063319d7
Felipe Cayres <felipescayres@gmail.com>
2024-12-13 17:16:55 -0500

    class 151

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -61,9 +61,94 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+/* 
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+*/
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+/* 
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE Method
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+// SPLICE
+// console.log(arr.splice(2))
+arr.splice(-1);
+console.log(arr);
+
+// REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+
+console.log(arr2.reverse());
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN
+
+console.log(letters.join('-'))
+
+// AT method
+const arr = [16,32,64]
+
+console.log(arr[0])
+console.log(arr.at(0))
+
+// getting array's last element
+console.log(arr[arr.length - 1])
+console.log(arr.slice(-1)[0])
+console.log(arr.at(-1))
+
+console.log('felipe'.at(0))
+console.log('felipe'.at(-1))
+
+
+
+// loops on arrays
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+movements.forEach(function (e, i, arr) {
+  let status = e > 0 ? 'deposited' : 'withdrew';
+  console.log(`Mov ${i}: You ${status} ${Math.abs(e)}\n${arr}`);
+});
+// forEach doesn't work with break/continue
+
+console.log('-------------------');
+//for (let mov of movements) {
+for (let [i, mov] of movements.entries()) {
+  if (mov > 0) {
+    console.log(`Movement ${i+1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i+1}: You withdrew ${Math.abs(mov)}`);
+  }
+}
+
+
+// forEach with map and set
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -71,6 +156,14 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+currencies.forEach((value, key, map) => {
+  console.log(`${key}: ${value}`);
+});
 
-/////////////////////////////////////////////////
+const currenciesUnique = new Set(['USD', 'GBP', 'EUR', 'EUR', 'USD']);
+console.log(currenciesUnique);
+currenciesUnique.forEach((value, _, map) => {
+  console.log(`${value}: ${value}`);
+});
+
+*/
