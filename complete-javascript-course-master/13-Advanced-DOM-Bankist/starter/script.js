a2a01b32c0ab04032211533b13bc4f713a6c1fca
Felipe Cayres <felipescayres@gmail.com>
2024-12-20 17:48:33 -0500

    passing arguments to event handlers

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -10,6 +10,14 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
+// Tabbed Components
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// Nav
+const nav = document.querySelector('.nav');
+
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -41,10 +49,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 // Tabbed Components
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
   // Get button and making sure is always catching the button.
   const clicked = e.target.closest('button');
@@ -59,14 +63,34 @@ tabsContainer.addEventListener('click', function (e) {
   clicked.classList.add('operations__tab--active');
 
   // Activate Content Area
-    // Remove classes from all elements
+  // Remove classes from all elements
   const currentContent = document.querySelector(
     `.operations__content--${clicked.dataset.tab}`
   );
-    // Adding class to target content element 
-  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
-  currentContent.classList.add('operations__content--active')
+  // Adding class to target content element
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  currentContent.classList.add('operations__content--active');
 });
+
+// Menu fade animation
+const handleHover = function (e) {
+  console.log(this);
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing 'argument' into handle functions
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+//
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
