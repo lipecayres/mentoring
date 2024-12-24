8cfa41869220a28e997e17b6267573cb9d0385d7
Felipe Cayres <felipescayres@gmail.com>
2024-12-23 20:44:54 -0500

    oop basic principles

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this :
+  // this.calcAge = function() {
+  //     console.log(2037 - birthYear);
+  // }
+};
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(matilda, jack);
+
+console.log(jack instanceof Person);
