6aeeed89e516570eaa8ae6a177dd21f24c675704
Felipe Cayres <felipescayres@gmail.com>
2024-12-27 00:10:57 -0500

    prototypes basic concepts

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -14,5 +14,27 @@ const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
 
 console.log(matilda, jack);
-
 console.log(jack instanceof Person);
+
+// Prototypes
+
+console.log(Person.prototype)
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jack.calcAge()
+matilda.calcAge()
+
+console.log(jack.__proto__);
+console.log(jack.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jack))
+console.log(Person.prototype.isPrototypeOf(matilda))
+console.log(Person.prototype.isPrototypeOf(Person))
+
+Person.prototype.species = 'Homo Sapiens'
+console.log(jack.species, matilda.species)
+console.log(jack.hasOwnProperty('firstName'))
+console.log(jack.hasOwnProperty('species'))
