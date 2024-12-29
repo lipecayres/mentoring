6959fb1c5720cf550a113dce3de56c5e7ab041c1
Felipe Cayres <felipescayres@gmail.com>
2024-12-29 10:17:53 -0500

    challenge 3 done

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -204,7 +204,7 @@ sarah.calcAge();
 
 //1.
 class Car {
-  constructor(make, speed) {
+constructor(make, speed) {
     this.make = make;
     this.speed = speed;
   }
@@ -237,7 +237,8 @@ ford.brake();
 ford.speedUS =50;
 console.log(ford)
 
-*/
+
+// Inheritance between classes
 
 const Person = function (fullName, birthYear) {
   this.fullName = fullName;
@@ -266,3 +267,48 @@ const mike = new Student('Mike', 2020, 'Computer Science');
 console.log(mike);
 mike.introduce();
 mike.calcAge();
+*/
+
+// Challenge 3
+
+// Constructor
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`The ${this.make} car is driving at ${this.speed}km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`The ${this.make} car is driving at ${this.speed}km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+// Linking prototypes
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.constructor = EV;
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--
+  console.log(`The ${this.make} car is driving at ${this.speed}km/h, with a charge of ${this.charge}%`);
+};
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.brake()
+tesla.accelerate()
+
